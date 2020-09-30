ea73f855ecc6a3b19b4c4976f4e36201b05c6147
Fikrifire19 <muhamadfikri64@yahoo.co.id>
2020-09-30 08:54:05 +0700

    membuat router

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
+
+router.use('/products', (req, res, next) => {
+    console.log('url: ', req.originalUrl);
+    console.log('method: ', req.method);
+    res.json({name: "Muhamad Fikri Mubarok", email: "muhamadfikri64@yahoo.co.id"});
+    next();
+})
+
+app.use('/', router);
 
 app.listen(4000);
