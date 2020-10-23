03bc3db1259debb4d3902559d789a7a0dda1ac2c
Fikrifire19 <muhamadfikri64@yahoo.co.id>
2020-10-23 22:33:52 +0700

    Membuat pagination data api

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -2,6 +2,7 @@ const {validationResult} = require('express-validator');
 const path = require('path');
 const fs = require('fs');
 const BlogPost = require('../models/blog');
+const { count } = require('console');
 
 exports.createBlogPost = (req, res, next) => {
     
@@ -45,16 +46,30 @@ exports.createBlogPost = (req, res, next) => {
 }
 
 exports.getAllPost = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = req.query.perPage || 5;
+    let totalItems;
+
     BlogPost.find()
+    .countDocuments()
+    .then(count => {
+        totalItems = count;
+        return BlogPost.find()
+        .skip((parseInt(currentPage) - 1) * parseInt(perPage))
+        .limit(parseInt(perPage));
+    })
     .then(result => {
         res.status(200).json({
             message: 'Data Blog Post Berhasil Dipanggil',
-            data: result
+            data: result,
+            total_data: totalItems,
+            per_page: parseInt(perPage),
+            current_page: parseInt(currentPage),
         })
     })
     .catch(err => {
         next(err);
-    });
+    })
 }
 
 exports.getPostById = (req, res, next) => {
